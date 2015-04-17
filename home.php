9868e361841bcf4885b36b063feac2d3286a1dd7
camilla-westin <camilla@webstin.se>
2015-04-17 15:14:38 +0200

    Added sidebar

--- home.php
+++ home.php
@@ -13,7 +13,7 @@
 
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-	<article class="postitem">
+	<article id="postitem post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 		<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 
@@ -34,6 +34,10 @@
 		<p><?php _e( 'Sorry, no posts matched your criteria.', 'c-framework' ); ?></p>
 
 	<?php endif; ?>
+
+	<aside class="sidebar blog-sidebar">
+		<?php dynamic_sidebar('blog-sidebar'); ?>
+	</aside>
 
 </section>
 
