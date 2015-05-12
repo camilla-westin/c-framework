847c359f62426a4b49b2713ff4667626fca9ab51
camilla-westin <camilla@webstin.se>
2015-05-12 08:40:01 +0200

    Moved code from functions to separate files

--- functions.php
+++ functions.php
@@ -110,35 +110,6 @@
 //This feature adds RSS feed links to HTML <head>.
 	add_theme_support( 'automatic-feed-links' );
 
-// Registers a new custom post showing contacts
-	add_action( 'init', 'contacts_post_type' );
-	function contacts_post_type() {
-	  register_post_type( 'contacts',
-	    array(
-	      'labels' => array(
-	        'name' => __( 'Contacts', 'c-framework' ),
-	        'singular_name' => __( 'Contact', 'c-framework' )
-	      ),
-	      'public' => true,
-	    )
-	  );
-	}
-
-
-
-//Changes the default "Enter title here" placeholder to "Enter name" for our contact custom posts
-	function change_contacts_title( $title ){
-	     $screen = get_current_screen();
-	 
-	     if  ( 'contacts' == $screen->post_type ) {
-	          $title = 'Enter name';
-	     }
-	 
-	     return $title;
-	}
-	 
-	add_filter( 'enter_title_here', 'change_contacts_title' );
-
 
 // Change length of excerpt
 	function cf_excerpt_length( $length ) {
@@ -147,60 +118,16 @@
 
 	add_filter( 'excerpt_length', 'cf_excerpt_length', 999 );
 
-
-//Add breadcrumbs
-
-	function the_breadcrumbs() {
- 
-        global $post;
- 
-        if (!is_home()) {
- 
-            echo "<a href='";
-            echo home_url();
-            echo "'>";
-            echo "Start";
-            echo "</a>";
- 
-            if (is_category() || is_single()) {
- 
-                echo " > ";
-                $cats = get_the_category( $post->ID );
- 
-                foreach ( $cats as $cat ){
-                    echo $cat->cat_name;
-                    echo " > ";
-                }
-                if (is_single()) {
-                    the_title();
-                }
-            } elseif (is_page()) {
- 
-                if($post->post_parent){
-                    $anc = get_post_ancestors( $post->ID );
-                    $anc_link = get_page_link( $post->post_parent );
- 
-                    foreach ( $anc as $ancestor ) {
-                        $output = " > <a href=".$anc_link.">".get_the_title($ancestor)."</a> > ";
-                    }
- 
-                    echo $output;
-                    the_title();
- 
-                } else {
-                    echo ' > ';
-                    echo the_title();
-                }
-            }
-        }
-    elseif (is_tag()) {single_tag_title();}
-    elseif (is_day()) {echo"Archive: "; the_time('F jS, Y'); echo'</li>';}
-    elseif (is_month()) {echo"Archive: "; the_time('F, Y'); echo'</li>';}
-    elseif (is_year()) {echo"Archive: "; the_time('Y'); echo'</li>';}
-    elseif (is_author()) {echo"Author's archive: "; echo'</li>';}
-    elseif (isset($_GET['paged']) && !empty($_GET['paged'])) {echo "Blogarchive: "; echo'';}
-    elseif (is_search()) {echo"Search results: "; }
+// Replaces the excerpt "more" text by a link
+function new_excerpt_more($more) {
+       global $post;
+	return ' <a class="moretag" href="'. get_permalink($post->ID) . '">Read more</a>';
 }
+add_filter('excerpt_more', 'new_excerpt_more');
 
 
-?>
+//Contacts - Custom post type
+	require_once('includes/contacts-customposttype.php');
+
+// Breadcrumbs
+	require_once('includes/breadcrumbs.php'); ?>
